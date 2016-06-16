d093f3dd1390267008e181cb13701d4da1b2c2cc
Guillaume Besson <Guillaume Besson>
2016-06-16 16:21:48 +0200

    Create controller classes

---
+++ JPU-BlankProject/controller/src/main/java/controller/UserOrder.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class UserOrder {
+	
+	private int player;
+	
+	public void controllerOrder(int player, Order order){
+		
+	}
+	
+	public Order getOrder(){
+		return null;
+		
+	}
+	
+	public static void getPlayer(){
+		
+	}
+
+}
