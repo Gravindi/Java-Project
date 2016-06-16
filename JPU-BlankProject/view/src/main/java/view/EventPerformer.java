dbf7d272701c82053f35fd2597b66fb690d11581
Guillaume Besson <Guillaume Besson>
2016-06-16 16:56:00 +0200

    Create View classes

---
+++ JPU-BlankProject/view/src/main/java/view/EventPerformer.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.awt.event.KeyEvent;
+
+import contract.IOrderPerformer;
+
+public class EventPerformer {
+	
+	public EventPerformer(IOrderPerformer orderPerformer){
+		
+	}
+	
+	public void eventperform(KeyEvent keyCode){
+		
+	}
+	
+	private UserOrder keyCodeToUserOrder(int keycode){
+		
+	}
+
+}
---
+++ JPU-BlankProject/view/src/main/java/view/Graphicsbuilder.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.image.ImageObserver;
+
+import contract.IModel;
+
+public class Graphicsbuilder {
+	
+	public void GraphicsBuilder (IModel model){
+		
+	}
+	
+	public void applyModelToGraphics(Graphics graphics, ImageObserver observer){
+		
+	}
+
+	public int getGlobalWidth(){
+		return 0;
+		
+	}
+	
+	public int getGlobalHeight(){
+		return 0;
+		
+	}
+	
+}
